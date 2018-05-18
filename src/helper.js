b4c24038715602ff871c71eedb4db3566ad34bbb
jo <fillion.jonathan@gmail.com>
2018-05-18 09:49:28 -0400

    server is running

--- src/helper.js
+++ src/helper.js
@@ -4,5 +4,78 @@ module.exports = {
       if(array[i].nonce === nonce) return false;}
     if(nonce < 16 && nonce > -1) return true;
     else return false;
+  },
+  loadStartingState : function (playersInfo) {
+    var confirmedSelections = []
+    for(var i = 0; i < playersInfo[0].length;i++) {
+      var selection = { address: playersInfo[0][i], x:0,y:0,nonce:playersInfo[1][i].toNumber()}
+      confirmedSelections.push(selection)
+    }
+    return confirmedSelections
+  },
+  addPendingField : function (pendingSelections) {
+    pendingSelections.forEach(function(obj) { obj.pending = true;});
+    return pendingSelections
+  },
+  //add game parameters
+  parsePlayEvent : function (playEvent, pendingSelections, confirmedSelections) {
+
+    var findIndexPending = function(address, pending) {
+      for(var i = 0 ; i < pending.length; i++){
+        if(pending[i].address == address) {
+          console.log("return i suppose");
+            return i;
+        }
+      }
+      return false;
+    }
+    var noRepetition = function (playEvent,confirmedSelections) {
+
+      for(var i = 0; i < confirmedSelections.length; i++) {
+        if(confirmedSelections[i].address == playEvent.address && confirmedSelections[i].nonce === playEvent.nonce ){
+          console.log('return false, already in');
+          return false
+        }
+      }
+      console.log("return true, not already in");
+      return true
+    }
+
+    var modifyArrays = function (playEvent, pendingSelections, confirmedSelections) {
+      if(noRepetition(playEvent, confirmedSelections)) {
+      var index = findIndexPending(playEvent.address, pendingSelections)
+      if (typeof index !== 'undefined') {
+        console.log(index + " objective");
+        confirmedSelections.push({address: pendingSelections[index].address, x:pendingSelections[index].x,y:pendingSelections[index].y,nonce:pendingSelections[index].nonce})
+        pendingSelections.splice(index,1)
+      } else {
+        //sinon ==> confirmedSelection with rdm x and y
+        confirmedSelections.push({address : playEvent.address, x:0, y:0, nonce : playEvent.nonce})
+      }}
+      return [pendingSelections, confirmedSelections]
+    }
+
+    if(confirmedSelections.length === 0) {
+      if(pendingSelections.length === 0) {
+        //In the case of a first new playGame
+        if(playEvent.counter == 1 ) {
+          confirmedSelections.push({address : playEvent.address, x:0, y:0, nonce : playEvent.nonce})
+        //old event or ambiguous if not maxNum
+        } else {
+          console.log("playEvent counter is not 1 :: 2 to maxNumberOfPlayers");
+          console.log(playEvent.counter);
+        }
+      //01
+      } else {
+        var results = modifyArrays(playEvent, pendingSelections, confirmedSelections)
+        pendingSelections = results[0]
+        confirmedSelections = results[1]
+      }
+    } else {
+        var results = modifyArrays(playEvent, pendingSelections, confirmedSelections)
+        pendingSelections = results[0]
+        confirmedSelections = results[1]
+    }
+    return [pendingSelections, confirmedSelections]
   }
 }
--- src/server.js
+++ src/server.js
@@ -8,20 +8,12 @@ var contract = require("truffle-contract");
 var fs = require("fs");
 var web3
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7546"));
-hashminerAbi = JSON.parse(fs.readFileSync("../build/contracts/Hashminer.json")).abi
-
-var hashminer = web3.eth.contract(hashminerAbi)
-var ins = hashminer.at("0x625b914E3836f1E477aE2E11f8537a94126b8139")
-console.log(ins.getGameInfo());
-
-
 
 app.use('/css',express.static(__dirname + '/css'));
 app.use('/js',express.static(__dirname + '/js'));
 app.use('/assets',express.static(__dirname + '/assets'));
 app.use('/fonts',express.static(__dirname + '/fonts'));
 //app.use('/',express.static(__dirname + '/'));
-
 
 app.get('/',function(req,res){
   res.sendFile(__dirname+'/index0.html');
@@ -34,41 +26,69 @@ server.listen(process.env.PORT || 8081,function(){
 var pendingSelections = []
 var confirmedSelections = [];
 var pendingTimer;
+var compteur = 0
+var latestPlayEvents = []
+var latestFinishEvents = []
+var hashminerAbi = JSON.parse(fs.readFileSync("../build/contracts/Hashminer.json")).abi
+var hashminer = web3.eth.contract(hashminerAbi)
+var instance = hashminer.at("0x625b914e3836f1e477ae2e11f8537a94126b8139")
+
+var PlayEvent = instance.LogPlayerAdded({},{fromBlock:"latest",toBlock:"latest"})
+PlayEvent.watch(function(err,res) {
+  if(!err){
+    var playEvent = {address:res.args._wallet.toLowerCase(),nonce:res.args._nonce.toNumber(),counter:res.args._playerCounter.toNumber()}
+    var result = helper.parsePlayEvent(playEvent, pendingSelections, confirmedSelections);
+    pendingSelections = result[0]
+    confirmedSelections = result[1]
+    console.log(JSON.stringify(pendingSelections));
+    console.log(JSON.stringify(confirmedSelections));
+  } else {
+    console.log(err)
+  }
+})
+
+var FinishEvent = instance.LogGameFinished({},{fromBlock:"latest", toBlock:"latest"})
+FinishEvent.watch(function(err,res) {
+  if(!err){
+    //Listen to winner, flush everything, it is confirmed.
+  } else {
+    console.log(err)
+  }
+})
+
+confirmedSelections = helper.loadStartingState(instance.getPlayersInfo())
 
 io.on('connection',function(socket){
-
   socket.on("gameLoaded",function(){
-    socket.emit("allPlayers", pendingSelections.concat(confirmedSelections));
 
+    socket.emit("allPlayers", confirmedSelections.concat(helper.addPendingField(pendingSelections)));
     socket.on("selectNonce", function(data){
       if(helper.nonceValid(pendingSelections.concat(confirmedSelections), data.nonce)){
-        pendingSelections.push(data)
-        pendingTimer = setInterval(helper.checkConfirmations(),2000);
+        console.log(JSON.stringify(data))
+        pendingSelections.push({address : data.address.toLowerCase(), x: parseInt(data.x), y: parseInt(data.y), nonce: parseInt(data.nonce)})
         socket.broadcast.emit("newSelection",pendingSelections.concat(confirmedSelections));
-      }
-      //--As long as pendingSelection has one element, listen to blockchain.
+        }
+      });
+    });
+    socket.on("debugPlayers", function(){
+      console.log(confirmedSelections.concat(helper.addPendingField(pendingSelections)))
+    })
 
+    socket.on("revealWinner", function(){
+      //add internal state stopping submission of button
+      if(true) {
+        socket.broadcast.emit("blockButton")
+        //watch ethereum to get the winner
+      }
     });
   });
-  socket.on("debugPlayers", function(){
-    console.log(JSON.stringify(pendingSelections.concat(confirmedSelections)))
-  })
 
-  socket.on("revealWinner", function(){
-    //add internal state stopping submission of button
-    if(true) {
-      socket.broadcast.emit("blockButton")
-      //watch ethereum to get the winner
+
+  function initWeb3() {
+    if (typeof web3 !== 'undefined') {
+      var web3 = new Web3(web3.currentProvider);
+    } else {
+      var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
     }
-  });
-});
-
-
-function initWeb3() {
-  if (typeof web3 !== 'undefined') {
-    var web3 = new Web3(web3.currentProvider);
-  } else {
-    var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+    return web3
   }
-  return web3
-}
